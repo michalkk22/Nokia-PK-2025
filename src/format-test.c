63ed8eed6eb801e94c17312ecfde4e268ddf5efc
PPF_LAB2 <jakubmak81@gmail.com>
2025-03-30 22:15:35 +0200

    clang-format-workflow

---
+++ src/format-test.c
@@ -0,0 +1,22 @@
+ #include <stdio.h>
+ 
+namespace Asdname {
+	
+	int bzxcbzf,zbf,zf,z;
+	}
+	
+	
+	int main()
+	{
+	int dupa= 5;
+	
+	if (1) {
+		dupa++;
+		}
+		if (dupa)
+		{
+			dupa--;
+		}
+		
+		return 0;
+	}
